597a0c69fdab6f2873f230f93b87c23f83dfc2de
Shrinivas A <ambatshri@users.noreply.github.com>
2023-01-24 23:58:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+var cors = require('cors')
 const app = express();
+
+app.use(cors())
+
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.send("POST request"));
