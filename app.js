7d2d4e8d2c2b51d077a045841b422e49ba6229e7
Shrinivas A <ambatshri@icloud.com>
2023-01-25 11:45:12 +0530

    Added Multer

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 var cors = require('cors')
 const app = express();
+const multer = require('multer');
+const upload = multer();
 
 app.use(cors())
 
+
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("POST request"));
+app.get("/", upload.none(), (req, res) => res.send("POST request"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
